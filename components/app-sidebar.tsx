86684e8e8e1c790f1f8445daac97f89628f76a05
Noureddine Kechroud <nouroukechroud@icloud.com>
2025-05-31 14:43:40 +0100

    Add sidebar and input components from shadcn using 'npx shadcn@latest add'.

---
+++ components/app-sidebar.tsx
@@ -0,0 +1,105 @@
+"use client"
+
+import * as React from "react"
+import {
+  Command, CreditCardIcon, FilePlus2Icon,
+  HandshakeIcon,
+  LifeBuoy, LogOutIcon, MoonIcon,
+  Send,
+  TablePropertiesIcon,
+} from "lucide-react"
+
+import { NavMain } from "@/components/nav-main"
+import { NavSecondary } from "@/components/nav-secondary"
+import { NavUser } from "@/components/nav-user"
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar"
+
+const data = {
+  user: {
+    name: "shadcn",
+    email: "m@example.com",
+    avatar: "/avatars/shadcn.jpg",
+  },
+  navMain: [
+    {
+      title: "New Request",
+      url: "/dashboard/new-request",
+      icon: FilePlus2Icon,
+      isActive: true,
+    },
+    {
+      title: "Transactions",
+      url: "/dashboard/transactions",
+      icon: TablePropertiesIcon,
+      isActive: true,
+    },
+    {
+      title: "Billing",
+      url: "/dashboard/billing",
+      icon: CreditCardIcon,
+      isActive: true,
+    },
+    {
+      title: "Referral",
+      url: "#",
+      icon: HandshakeIcon,
+      isActive: true,
+    },
+  ],
+  navSecondary: [
+    {
+      title: "Support",
+      url: "#",
+      icon: LifeBuoy,
+    },
+    {
+      title: "Feedback",
+      url: "#",
+      icon: Send,
+    },
+    {
+      title: "Dark Mode",
+      url: "#",
+      icon: MoonIcon,
+    },
+  ],
+}
+
+export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+  return (
+    <Sidebar variant="inset" {...props}>
+      <SidebarHeader>
+        <SidebarMenu>
+          <SidebarMenuItem>
+            <SidebarMenuButton size="lg" asChild>
+              <a href="#">
+                <div className="bg-sidebar-primary text-sidebar-primary-foreground flex aspect-square size-8 items-center justify-center rounded-lg">
+                  <Command className="size-4" />
+                </div>
+                <div className="grid flex-1 text-left text-sm leading-tight">
+                  <span className="truncate font-medium">Acme Inc</span>
+                  <span className="truncate text-xs">Enterprise</span>
+                </div>
+              </a>
+            </SidebarMenuButton>
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarHeader>
+      <SidebarContent>
+        <NavMain items={data.navMain} />
+        <NavSecondary items={data.navSecondary} className="mt-auto" />
+      </SidebarContent>
+      <SidebarFooter>
+        <NavUser user={data.user} />
+      </SidebarFooter>
+    </Sidebar>
+  )
+}
